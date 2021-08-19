aa54f20bef9c4b848991090077cba315f166f817
diddy032 <gwul3vu032@gmail.com>
2021-08-19 23:14:34 +0800

    audio bar ui 雛形

--- src/components/albumCard.js
+++ src/components/albumCard.js
@@ -1,19 +1,15 @@
 import React from "react";
 import {
-  createTheme,
   withStyles,
   makeStyles,
-  ThemeProvider,
 } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import { borders } from "@material-ui/system";
 import { orange } from "@material-ui/core/colors";
 
 const useStyles = makeStyles({
--- src/components/audioBar.js
+++ src/components/audioBar.js
@@ -1,13 +1,94 @@
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
+
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  content: {
+    flex: "1 0 auto",
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+}));
+
 export default function AudioBar() {
+  const classes = useStyles();
+  const theme = useTheme();
   return (
-    <div>
+    <Container fixed maxWidth={false}>
+      <Card className={classes.root}>
+        <CardMedia
+          className={classes.cover}
+          image="/static/images/cards/live-from-space.jpg"
+          title="Live from space album cover"
+        />
+        <div className={classes.details}>
+          <CardContent className={classes.content}>
+            <Typography component="h5" variant="h5">
+              Live From Space
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              Mac Miller
+            </Typography>
+          </CardContent>
+          <div className={classes.controls}>
+            <IconButton aria-label="previous">
+              {theme.direction === "rtl" ? (
+                <SkipNextIcon />
+              ) : (
+                <SkipPreviousIcon />
+              )}
+            </IconButton>
+            <IconButton aria-label="play/pause">
+              <PlayArrowIcon className={classes.playIcon} />
+            </IconButton>
+            <IconButton aria-label="next">
+              {theme.direction === "rtl" ? (
+                <SkipPreviousIcon />
+              ) : (
+                <SkipNextIcon />
+              )}
+            </IconButton>
+          </div>
+        </div>
+      </Card>
+      {/* <Box component="div" bgcolor="text.primary" color="background.paper">
+        great pleasure <br /> Richard Green
+      </Box> */}
       {/* <AudioPlayer
-        autoPlay
+        autoPlay={false}
         src="/audio/URL Melt - Unicorn Heads.mp3"
         onPlay={(e) => console.log("onPlay")}
       /> */}
-    </div>
+    </Container>
   );
 }
